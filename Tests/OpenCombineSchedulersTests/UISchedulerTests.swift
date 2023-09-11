7e285a6a2dff23d15bffb87ab5a27dfe6e828ab1
Brian Michel <brian.michel@gmail.com>
2023-09-11 17:55:22 -0400

    Add imports and gets tests running

--- Tests/OpenCombineSchedulersTests/UISchedulerTests.swift
+++ Tests/OpenCombineSchedulersTests/UISchedulerTests.swift
@@ -1,33 +1,31 @@
-#if canImport(Combine)
-  import Combine
-  import OpenCombineSchedulers
-  import XCTest
+import OpenCombineShim
+import OpenCombineSchedulers
+import XCTest
 
-  final class UISchedulerTests: XCTestCase {
-    func testVoidsThreadHop() {
-      var worked = false
-      UIScheduler.shared.schedule { worked = true }
-      XCTAssert(worked)
-    }
-
-    func testRunsOnMain() {
-      let queue = DispatchQueue.init(label: "queue")
-      let exp = self.expectation(description: "wait")
-
-      var worked = false
-      queue.async {
-        XCTAssert(!Thread.isMainThread)
-        UIScheduler.shared.schedule {
-          XCTAssert(Thread.isMainThread)
-          worked = true
-          exp.fulfill()
-        }
-        XCTAssertFalse(worked)
-      }
-
-      self.wait(for: [exp], timeout: 1)
-
-      XCTAssertTrue(worked)
-    }
+final class UISchedulerTests: XCTestCase {
+  func testVoidsThreadHop() {
+    var worked = false
+    UIScheduler.shared.schedule { worked = true }
+    XCTAssert(worked)
   }
-#endif
+
+  func testRunsOnMain() {
+    let queue = DispatchQueue.init(label: "queue")
+    let exp = self.expectation(description: "wait")
+
+    var worked = false
+    queue.async {
+      XCTAssert(!Thread.isMainThread)
+      UIScheduler.shared.schedule {
+        XCTAssert(Thread.isMainThread)
+        worked = true
+        exp.fulfill()
+      }
+      XCTAssertFalse(worked)
+    }
+
+    self.wait(for: [exp], timeout: 1)
+
+    XCTAssertTrue(worked)
+  }
+}
